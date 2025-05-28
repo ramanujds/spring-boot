5d64974cbf0ffc4ec028fd1a0f486bbbc4e4ee29
Ramanuj Das <ramanujds9@gmail.com>
2025-05-28 16:39:22 +0530

    Updated with simple api

---
+++ src/main/java/com/rds/app/api/GreetController.java
@@ -0,0 +1,26 @@
+package com.rds.app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+public class GreetController
+{
+
+    @GetMapping
+    public Map<String, String> greet()
+    {
+        return Map.of(
+            "message", "Congratulations! You have successfully deployed your first Spring Boot application on AWS",
+            "status", "success",
+            "timestamp", LocalDateTime.now().toString(),
+            "version", "1.0.0",
+            "author", "Ramanuj Das"
+        );
+
+    }
+
+}
